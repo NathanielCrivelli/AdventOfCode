27ecda18b19d54cc13b67a103b18a5734fb98287
Ty Gagielo <ty.gagielo@mrjaffesclass.com>
2023-10-13 14:24:33 -0700

    force time

--- AdventOfCode/AdventOfCode/Day4/Part1.java
+++ AdventOfCode/AdventOfCode/Day4/Part1.java
@@ -1,15 +1,18 @@
 package Day4;
 
 public class Part1 {
-    protected static final String ipt = Input.GetInput4();
+    protected static String ipt = Input.GetInput4();
     private static String temp = "";
-    private static int currentBegin = 0, currentEnd = -1;
+    private static String str1 = "";
+    private static String str2 = "";
+    private static int currentBegin = 0, currentEnd = -1, counter;
 
-    private static int[][] arr = new int[2][2];
+    private static int frontone, fronttwo, backone, backtwo = 0;
 
     public static void main(String[] args){
         System.out.println("start");
         loopers();
+        System.out.println(counter);
     }
 
     private static void loopers(){
@@ -19,31 +22,45 @@ public class Part1 {
                 currentBegin = currentEnd+1;
                 currentEnd = i;
                 temp = ipt.substring(currentBegin, currentEnd);
-                System.out.println(temp);
+
                 makeSubs(temp);
+
+                frontone = Integer.valueOf(str1);
+                fronttwo = Integer.valueOf(str2);
+
+                makeSubs(temp);
+                
+                backone = Integer.valueOf(str1);                
+                backtwo = Integer.valueOf(str2);
+
+                counter = (logic())? counter+1 : counter;
             }
         }
+    }
+
+    private static boolean logic(){
+        if(frontone >= backone && backtwo >= fronttwo){
+            return true;
+        }
+        else if(backone >= frontone && fronttwo >= backtwo){
+            return true;
+        }
+
+        else if(frontone >= backone && backtwo <= fronttwo && frontone <= backtwo){
+            return true;
+        }
+        else if(backone >= frontone && fronttwo >= backone && backtwo >= fronttwo){
+            return true;
+        }
+
+        return false;
     }
 
     private static void makeSubs(String segment){
-        segment.replace(',', '\n');
-        segment.replace('-', '\n');
-        
-        int previous=0;
-        
-        for (int i=0; i<segment.length(); i++){
+        str1 = segment.substring(0, segment.indexOf('-'));
+        str2 = segment.substring(segment.indexOf('-')+1, segment.indexOf(','));
 
-            if(segment.charAt(i) == '\n'){
-                temp = segment.substring(previous, i);
-                arr[0][0] = getInts(temp);
-                System.out.println(arr[0][0]);
-                previous=i+1;
-            }
-        }
-
+        temp = segment.substring(segment.indexOf(',')+1, segment.length())+",";
     }
-
-    private static int getInts(String segmentSegment){
-        return Integer.parseInt(segmentSegment);
-    }
+    
 }
