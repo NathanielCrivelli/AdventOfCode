442f7192a6076a7f8c2020497171c60e15a48c43
TyGagielo <112726808+TyGagielo@users.noreply.github.com>
2023-10-16 14:30:03 -0700

    commit

---
+++ Day6/Part.java
@@ -0,0 +1,18 @@
+package Day6;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Part {
+
+    private static Map<Character, Integer> map= new HashMap<Character, Integer>();
+
+    private static String inpt = Input.GetInput6();
+    public static void main(String args[]){
+
+    }
+
+    public static void loopthru(){
+        
+    }
+}
