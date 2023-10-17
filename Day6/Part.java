d29a11d3ffc40343caa34d511fb1c643630ce0f4
TyGagielo <112726808+TyGagielo@users.noreply.github.com>
2023-10-17 14:25:24 -0700

    Update Part.java

--- Day6/Part.java
+++ Day6/Part.java
@@ -3,16 +3,38 @@ package Day6;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Part {
+public class Parts {
 
     private static Map<Character, Integer> map= new HashMap<Character, Integer>();
 
-    private static String inpt = Input.GetInput6();
-    public static void main(String args[]){
+    private static String sub = "";
 
+    private static int IMPORTANTNUMBER = 14, answer = 0;
+
+    private static String inpt = Input.getInput6();
+    public static void main(String args[]){
+        loopthru();
+        System.out.println(answer+IMPORTANTNUMBER);
     }
 
-    public static void loopthru(){
-        
+    private static void loopthru(){
+        for(int i=0; i < inpt.length()-IMPORTANTNUMBER; i++){
+            sub = inpt.substring(i, i+IMPORTANTNUMBER);
+            makeHash(sub);
+
+            if(map.size() >= IMPORTANTNUMBER){
+                answer = i;
+                break;
+            }
+        }
+    }
+
+    private static void makeHash(String s){
+        map.clear();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
     }
 }
